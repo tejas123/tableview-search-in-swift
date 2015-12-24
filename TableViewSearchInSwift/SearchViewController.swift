52264ff3e0e70e28371f62d20b723a8d48e35d4f
Bharat <bharatappguru@gmail.com>
2015-12-24 11:10:27 +0530

    Updated

--- TableViewSearchInSwift/SearchViewController.swift
+++ TableViewSearchInSwift/SearchViewController.swift
@@ -1,26 +1,30 @@
 //
-//  SearchTableViewController.swift
+//  SearchViewController.swift
 //  TableViewSearchInSwift
 //
-//  Created by TheAppGuruz-iOS-103 on 07/02/15.
-//  Copyright (c) 2015 TheAppGururz. All rights reserved.
+//  Created by TheAppGuruz-iOS-103 on 24/12/15.
+//  Copyright © 2015 TheAppGururz. All rights reserved.
 //
 
 import UIKit
 
-class SearchTableViewController: UITableViewController, UISearchDisplayDelegate, UISearchBarDelegate
+class SearchViewController: UIViewController, UITableViewDelegate, UISearchDisplayDelegate, UISearchBarDelegate
 {
+
     var marrCountryList = [String]()
     var marrFilteredCountryList = [String]()
+    
+    @IBOutlet var tblCountry: UITableView!
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
+
         marrCountryList = ["USA", "Bahamas", "Brazil", "Canada", "Republic of China", "Cuba", "Egypt", "Fiji", "France", "Germany", "Iceland", "India", "Indonesia", "Jamaica", "Kenya", "Madagascar", "Mexico", "Nepal", "Oman", "Pakistan", "Poland", "Singapore", "Somalia", "Switzerland", "Turkey", "UAE", "Vatican City"]
-        self.tableView.reloadData()
+        self.tblCountry.reloadData()
     }
     
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
         if tableView == self.searchDisplayController!.searchResultsTableView {
             return self.marrFilteredCountryList.count
@@ -28,10 +32,10 @@ class SearchTableViewController: UITableViewController, UISearchDisplayDelegate,
             return self.marrCountryList.count
         }
     }
-
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
     {
-        let cellCountry = self.tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
+        let cellCountry = self.tblCountry.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
         
         var countryName : String!
         if tableView == self.searchDisplayController!.searchResultsTableView {
@@ -46,10 +50,11 @@ class SearchTableViewController: UITableViewController, UISearchDisplayDelegate,
     }
     
     func filterTableViewForEnterText(searchText: String) {
-        self.marrFilteredCountryList = self.marrCountryList.filter({( strCountry : String) -> Bool in
-            let stringForSearch = strCountry.rangeOfString(searchText)
-            return (stringForSearch != nil)
-        })
+        let searchPredicate = NSPredicate(format: "SELF CONTAINS[c] %@", searchText)
+        
+        let array = (self.marrCountryList as NSArray).filteredArrayUsingPredicate(searchPredicate)
+        self.marrFilteredCountryList = array as! [String]
+        self.tblCountry.reloadData()
     }
     
     func searchDisplayController(controller: UISearchDisplayController, shouldReloadTableForSearchString searchString: String?) -> Bool {
@@ -62,5 +67,5 @@ class SearchTableViewController: UITableViewController, UISearchDisplayDelegate,
             self.filterTableViewForEnterText(self.searchDisplayController!.searchBar.text!)
             return true
     }
-    
+
 }
